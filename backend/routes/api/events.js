adfab865e99a94e975600def6ceefba733cd303e
Ryan Kagrise <rdkcode@gmail.com>
2022-03-08 12:32:06 -1000

    working on showing all events

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -3,4 +3,17 @@ const asyncHandler = require('express-async-handler');
 
 const { Event } = require('../../db/models');
 
+const eventValidators = require('../../validations/events')
 
+const router = express.Router();
+
+router.get(
+  '/',
+  asyncHandler(async function(_req, res) {
+    const events = await Event.findAll();
+    return res.json(events);
+  })
+);
+
+
+module.exports = router;
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,9 +1,29 @@
-import { NavLink } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { NavLink, useParams } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react'
 import "./SplashPage.css"
+
+import { fetchEvents } from '../../store/event';
 
 const SplashPage = () => {
   const sessionUser = useSelector(state => state.session.user);
+
+  const dispatch = useDispatch();
+  const { eventId } = useParams();
+
+  const events = useSelector(state => {
+    return state.events.list.map(eventId => state.events[eventId]);
+  });
+
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch]);
+
+  if (!events) {
+    return null;
+  }
+
+
 
   if(sessionUser) {
     return (
@@ -14,8 +34,28 @@ const SplashPage = () => {
               <p className='title'> Welcome to Dive Events! </p>
               <img className='tank-photo' src="/images/scuba-background.jpg" alt=''/>
               <p className='info-p'> Please look below for upcoming diving events! </p>
+              <div className='events-links'>
+                {events.map(event => {
+                  return (
+                    <NavLink key={event.name} to={`/events/${event.id}`}>
+                      <div
+                        className={
+                          Number.parseInt(eventId) === event.id
+                            ? 'nav-entry is-selected'
+                            : 'nav-entry'
+                        }
+                      >
+                        <div>
+                          <div className='event-name'>{event.name}</div>
+                          <div className='event-date'>{event.date}</div>
+                        </div>
+                      </div>
+                    </NavLink>
+                  )
+                })}
 
-          </div>
+              </div>
+            </div>
               <img className='beach-background' src="/images/beach-background.jpg" alt=''/>
         </div>
 
